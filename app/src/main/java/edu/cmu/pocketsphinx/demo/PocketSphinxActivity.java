5e6dfb44c6e8191b285899bc3111f03384c00e9f
Linus Schaub <linus@5dlab.com>
2017-06-11 15:11:00 +0200

    Fixed recognization

--- app/src/main/java/edu/cmu/pocketsphinx/demo/PocketSphinxActivity.java
+++ app/src/main/java/edu/cmu/pocketsphinx/demo/PocketSphinxActivity.java
@@ -47,7 +47,6 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 
 import edu.cmu.pocketsphinx.Assets;
 import edu.cmu.pocketsphinx.Hypothesis;
@@ -62,15 +61,17 @@ public class PocketSphinxActivity extends Activity implements
 
 
     /* Keyword we are looking for to activate menu */
-    private static String keyword = "ale4f3o40a";
-    private static float keywordThreshold = 0.5f;
+    private static String keyword = "alexa";
+    private static float keywordThreshold = 50f;
     private static String KWS_SEARCH = "Default search";
+    public boolean isStarting = false;
+    public boolean hasUpdate = false;
+    private static String debugPrefix = "earbuddy ";
 
     /* Used to handle permission request */
     private static final int PERMISSIONS_REQUEST_RECORD_AUDIO = 1;
 
     private SpeechRecognizer recognizer;
-    private HashMap<String, Integer> captions;
 
     @Override
     public void onCreate(Bundle state) {
@@ -80,16 +81,16 @@ public class PocketSphinxActivity extends Activity implements
         setContentView(R.layout.main);
        // ((TextView) findViewById(R.id.caption_text))
         //        .setText("Preparing the recognizer");
-
+        Log.d(debugPrefix + "Init","Checking permisssion");
         // Check if user has given permission to record audio
         int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO);
         if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+
+            Log.d(debugPrefix + "Init","Requesting permisssion");
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSIONS_REQUEST_RECORD_AUDIO);
             return;
         }
-
         keyword = ((EditText) findViewById(R.id.et_name)).getText().toString();
-
         runRecognizerSetup();
         addListeners();
     }
@@ -99,9 +100,8 @@ public class PocketSphinxActivity extends Activity implements
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                // todo take value and restart recognizer
                 keywordThreshold = seekBar.getProgress();
-                Log.d("OnChangedThreshold",seekBar.getProgress()+" ");
+                Log.d(debugPrefix + "New Threshold","Changed threshold bar to "+seekBar.getProgress()+" ");
                 changedRecognizerSettings();
             }
 
@@ -122,7 +122,7 @@ public class PocketSphinxActivity extends Activity implements
             @Override
             public void afterTextChanged(Editable s) {
                 keyword = s.toString();
-                Log.d("OnChangedKeyword", "Changed keyword to "+keyword);
+                Log.d(debugPrefix + "ChangedKeyword", "Changed keyword to "+keyword);
                 changedRecognizerSettings();
             }
 
@@ -144,8 +144,16 @@ public class PocketSphinxActivity extends Activity implements
     public void changedRecognizerSettings() {
         Log.d("OnChangedValue", "Restarting recognizer with keyword "+keyword+" and threshold of "+keywordThreshold);
         setStatusText("Restarting...");
-        destroyRecognizer();
-        runRecognizerSetup();
+        restartRecognizer();
+    }
+
+    public void restartRecognizer() {
+        if(isStarting == false) {
+            destroyRecognizer();
+            runRecognizerSetup();
+        } else {
+            hasUpdate = true;
+        }
     }
 
     public void setStatusText(String s) {
@@ -155,8 +163,9 @@ public class PocketSphinxActivity extends Activity implements
     private void runRecognizerSetup() {
         // Recognizer initialization is a time-consuming and it involves IO,
         // so we execute it in async task
+        isStarting = true;
 
-        Log.d("Setup", "Running recognizer setup");
+        Log.d(debugPrefix + "Setup", "Running recognizer setup");
 
         new AsyncTask<Void, Void, Exception>() {
             @Override
@@ -174,13 +183,27 @@ public class PocketSphinxActivity extends Activity implements
             @Override
             protected void onPostExecute(Exception result) {
                 if (result != null) {
+                    isStarting = false;
+                    if(hasUpdate) {
+                        hasUpdate = false;
+                        Log.d(debugPrefix + "Setup", "Restarting recognizer");
+                        restartRecognizer();
+                    }
                     if(result.getMessage().contains("Microphone")){
-                        destroyRecognizer();
-                        runRecognizerSetup();
+                        //restartRecognizer();
                         return;
                     }
                     setStatusText("Error: " + result.getMessage());
                 } else {
+                    isStarting = false;
+                    if(hasUpdate) {
+                        hasUpdate = false;
+                        restartRecognizer();
+                    }
+                    if(recognizer != null) {
+                        recognizer.startListening(KWS_SEARCH);
+                    }
+                    Log.d(debugPrefix + "Setup", "Successfully started recognizer");
                     setStatusText("Recognizer is running");
                 }
             }
@@ -191,30 +214,33 @@ public class PocketSphinxActivity extends Activity implements
     private void setupRecognizer(File assetsDir) throws IOException {
         // The recognizer can be configured to perform multiple searches
         // of different kind and switch between them
-        Log.d("Setup", "Setting up recognizer");
 
         if(keyword.length()==0) {
-            Log.e("Setup", "No keyword set, not setting up recognizer");
+            Log.e(debugPrefix + "Setup", "No keyword set, not setting up recognizer");
             throw new IOException("Keyword required");
         }
+
+        float calculatedThreshold = (float) Math.pow(10f,(100f-keywordThreshold)/100f*-38f);
 
         recognizer = SpeechRecognizerSetup.defaultSetup()
                 .setAcousticModel(new File(assetsDir, "en-us-ptm"))
                 .setDictionary(new File(assetsDir, "cmudict-en-us.dict"))
-                .setKeywordThreshold(keywordThreshold)
+                .setKeywordThreshold(calculatedThreshold)
+                .setFloat("-beam",calculatedThreshold)
+
                 //.setRawLogDir(assetsDir) // To disable logging of raw audio comment out this call (takes a lot of space on the device)
 
                 .getRecognizer();
+
         String[] words = keyword.split("\\s+");
         for( int i=0; i<words.length; i++) {
             if(recognizer.getDecoder().lookupWord(words[i].toLowerCase()) == null) {
-                Log.e("Setup", "Could not find keyword \""+words[i]+"\" in dictionary");
+                Log.e(debugPrefix + "Setup", "Could not find keyword \""+words[i]+"\" in dictionary");
                 throw new IOException("Name "+words[i]+" not found in dictionary");
             }
         }
 
         recognizer.addListener(this);
-
         // Create keyword-activation search.
         recognizer.addKeyphraseSearch(KWS_SEARCH, keyword.toLowerCase());
     }
@@ -226,9 +252,9 @@ public class PocketSphinxActivity extends Activity implements
 
         if (requestCode == PERMISSIONS_REQUEST_RECORD_AUDIO) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                runRecognizerSetup();
+                restartRecognizer();
             } else {
-                Log.e("Recognizer Setup", "Record audio permission denied by user");
+                Log.e(debugPrefix + "Setup", "Record audio permission denied by user");
                 finish();
             }
         }
@@ -242,14 +268,16 @@ public class PocketSphinxActivity extends Activity implements
 
     public void destroyRecognizer() {
         if (recognizer != null) {
+            recognizer.stop();
+            recognizer.removeListener(this);
             recognizer.cancel();
-            recognizer.shutdown();
+            recognizer = null;
         }
     }
 
     @Override
     public void onBeginningOfSpeech() {
-        Log.d("Recognization","Beginning of speech");
+        /*Log.d(debugPrefix + "Recognization","Beginning of speech");*/
     }
 
     /**
@@ -259,14 +287,16 @@ public class PocketSphinxActivity extends Activity implements
      */
     @Override
     public void onPartialResult(Hypothesis hypothesis) {
-        if (hypothesis == null)
+        if (hypothesis == null){
             return;
+        }
 
         String text = hypothesis.getHypstr();
-        Log.d("Recognization","Partial result "+text);
-        if (text.equals(keyword)) {
-            ((TextView) findViewById(R.id.status_text)).setText(text);
-            Log.d("Recognization", "Got keyword in partial result");
+        if (text.equals(keyword.toLowerCase())&& recognizer != null) {
+            recognizer.stop();
+            recognizer.startListening(KWS_SEARCH);
+            Log.d(debugPrefix + "Recognization", "Recognized keyword \""+keyword+"\"");
+            makeText(getApplicationContext(), "Keyword \""+keyword+"\" spotted", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -278,10 +308,8 @@ public class PocketSphinxActivity extends Activity implements
         //((TextView) findViewById(R.id.status_text)).setText("");
         if (hypothesis != null) {
             String text = hypothesis.getHypstr();
-            Log.d("Recognization","Result "+text);
-            makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
         } else {
-            Log.d("Recognization","No result");
+
         }
     }
 
@@ -290,19 +318,20 @@ public class PocketSphinxActivity extends Activity implements
      */
     @Override
     public void onEndOfSpeech() {
-        Log.d("Recognization","End of speech");
+        /*Log.d(debugPrefix + "Recognization","End of speech");*/
     }
 
 
     @Override
     public void onError(Exception error) {
-        Log.d("Setup", "Got error setting up recognizer");
+        Log.d(debugPrefix + "Setup", "Got error setting up recognizer "+error.getMessage());
         ((TextView) findViewById(R.id.status_text)).setText(error.getMessage());
+        restartRecognizer();
     }
 
     @Override
     public void onTimeout() {
-        Log.d("OnTimeout", "Recognizer timed out.");
+        Log.d(debugPrefix + "OnTimeout", "Recognizer timed out.");
         recognizer.startListening(KWS_SEARCH);
     }
 }
